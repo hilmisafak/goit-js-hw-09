320f517f10947786fb99b69ba7d49acb857c0879
HilmiSafak <hilmisafak1@gmail.com>
2025-09-06 20:05:56 +0300

    Refactoring

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,3 @@
-import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
 import { images } from "./gallery-items.js";
 
 const galleryContainer = document.querySelector(".gallery");
